d3a2da2d1c21322462284c03b77e689ba683c33f
MF_sy <sytoby@163.com>
2017-01-09 11:06:56 +0800

    sd

---
+++ sEasyBeautifyImageModel/sEasyBeautifyImageModel/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  sEasyBeautifyImageModel
+//
+//  Created by mofang2 on 17/1/9.
+//  Copyright © 2017年 com.cfd.MoFang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ sEasyBeautifyImageModel/sEasyBeautifyImageModel/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  sEasyBeautifyImageModel
+//
+//  Created by mofang2 on 17/1/9.
+//  Copyright © 2017年 com.cfd.MoFang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
